5e4f0def7395edd3ebd247c1d6f6c520fa2f9382
Bikash Rai <brai@xlevel-tech.com>
2014-01-01 22:24:56 -0800

    Chapter 6 completed but still image upload is not working. need to visit again and fix it

--- source-client/module/Wall/src/Wall/Controller/IndexController.php
+++ source-client/module/Wall/src/Wall/Controller/IndexController.php
@@ -13,7 +13,10 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Stdlib\Hydrator\ClassMethods;
 use Users\Entity\User;
 use Wall\Forms\TextStatusForm;
+use Wall\Forms\ImageForm;
 use Wall\Entity\Status;
+use Zend\Validator\File\Size;
+use Zend\Validator\File\IsImage;
 use Api\Client\ApiClient as ApiClient;
 
 class IndexController extends AbstractActionController
@@ -40,6 +43,7 @@ class IndexController extends AbstractActionController
         //Check if we are submitting content
         $request = $this->getRequest();
         $statusForm = new TextStatusForm;
+        $imageForm = new ImageForm();
 
         if ($request->isPost()) {
             $data = $request->getPost()->toArray();
@@ -48,9 +52,20 @@ class IndexController extends AbstractActionController
                 $result = $this->createStatus($statusForm, $user, $data);
             }
 
+            if (!empty($request->getFiles()->image)) {
+                $data = array_merge_recursive(
+                    $data,
+                    $request->getFiles()->toArray()
+                );
+                $result = $this->createImage($imageForm, $user, $data);
+            }
+
             switch (true) {
                 case $result instanceOf TextStatusForm:
                     $statusForm = $result;
+                    break;
+                case $result instanceOf ImageForm:
+                    $imageForm = $result;
                     break;
                 default:
                     if ($result == true) {
@@ -64,14 +79,81 @@ class IndexController extends AbstractActionController
         }
 
         $statusForm->setAttribute('action', $this->url()->fromRoute('wall', array('username' => $user->getUsername())));
+        $imageForm->setAttribute('action', $this->url()->fromRoute('wall', array('username' => $user->getUsername())));
         $viewData['profileData'] = $user;
         $viewData['textContentForm'] = $statusForm;
+        $viewData['imageContentForm'] = $imageForm;
 
         if ($flashMessenger->hasMessages()) {
             $viewData['flashMessages'] = $flashMessenger->getMessages();
         }
 
         return $viewData;
+    }
+
+    /**
+     * Upload a new image
+     *
+     * @param Zend\Form\Form $form
+     * @param Users\Entity\User $user
+     * @param array $data
+     */
+    protected function createImage($form, $user, $data)
+    {
+        if ($data['image']['error'] != 0) {
+            $data['image'] = NULL;
+        }
+
+        $form->setData($data);
+
+        $size = new Size(array('max' => 2048000));
+        $isImage = new IsImage();
+        $filename = $data['image']['name'];
+
+        $adapter = new \Zend\File\Transfer\Adapter\Http();
+        $adapter->setValidators(array($size, $isImage), $filename);
+
+        if (!$adapter->isValid($filename)){
+            $errors = array();
+            foreach($adapter->getMessages() as $key => $row) {
+                $errors[] = $row;
+            }
+            $form->setMessages(array('image' => $errors));
+        }
+
+        if ($form->isValid()) {
+            $destPath = "/var/source-client/data/tmp/";
+            $adapter->setDestination($destPath);
+
+            $fileinfo = $adapter->getFileInfo();
+            preg_match('/.+\/(.+)/', $fileinfo['image']['type'], $matches);
+            $extension = $matches[1];
+            $newFilename = sprintf('%s.%s', sha1(uniqid(time(), true)), $extension);
+
+            $adapter->addFilter('File\Rename',
+                array(
+                    'target' => $destPath . $newFilename,
+                    'overwrite' => true,
+                )
+            );
+
+            if ($adapter->receive($filename)) {
+                $data = array();
+                $data['image'] = base64_encode(
+                    file_get_contents(
+                        $destPath . $newFilename
+                    )
+                );
+                $data['user_id'] = $user->getId();
+
+                unlink($destPath . $newFilename);
+
+                $response = ApiClient::postWallContent($user->getUsername(), $data);
+                return $response['result'];
+            }
+        }
+
+        return $form;
     }
 
     /**
